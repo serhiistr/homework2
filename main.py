fe7ec7a884bbdad4fe6d067aef244edc1d18f477
serhiistr <sergaudit2019@gmail.com>
2022-10-22 00:10:46 +0300

    22102022 added methods

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from models import Toyota
 
 while True:
-    print("1. Add engine, color and shift gir car")
-    print("2. Oll cars Toyota in list: ")
+    print()
+    print("1. Add model, engine, color and shift gir car: ")
+    print("2. All cars Toyota in list: ")
+    print("3. Choose car by engine: ")
+    print("4. Change color: ")
+    print("5. Quit: ")
 
     flag = int(input("Chose menu item: "))
 
     if flag == 1:
-        engine = float(input("engine in cm3 (example - 1.5): "))
+        name = input("Car model: ")
+        engine = input("Enter the engine (diesel or petrol): ")
         color = input("Add color car: ")
         shift_gir = input("Add shit gir for car - automatic or manual: ")
-        toyota = Toyota(engine, color, shift_gir)
+        toyota = Toyota(name, engine, color, shift_gir)
         toyota.save()
 
     elif flag == 2:
         Toyota.get_all_cars()
+
+    elif flag == 3:
+        Toyota.get_car_engine()
+
+    elif flag == 4:
+        Toyota.change_color_car()
+
+    elif flag == 5:
+        break
+
+
--- models.py
+++ models.py
@@ -1,11 +1,11 @@
-import  json
+import json
+
 
 class Toyota:
     file = "toyota.json"
-    # name = "Toyota"
 
-    def __init__(self, engine, color, shift_gir):
-        # self.name = name
+    def __init__(self, name, engine, color, shift_gir):
+        self.name = name
         self.engine = engine
         self.color = color
         self.shift_gir = shift_gir
@@ -22,11 +22,27 @@ class Toyota:
     def get_all_cars(cls):
         data = cls.get_data()
         for car in data:
-            print(car["engine"], car["color"], car["shift_gir"])
+            print(car["id"], car["name"], car["engine"], car["color"], car["shift_gir"])
+
+
+    @classmethod
+    def get_by_id(cls, id):
+        data = cls.get_data()
+        if len(data) > 0:
+            fields = data[0].keys()
+            for el in data:
+                if id == el["id"]:
+                    for field in fields:
+                        print(el[field])
+                    break
+                elif id != len(el):
+                    print("Not found car ID")
+                    break
 
     def save(self):
         data = self.get_data()
-        new_car = {"engine": self.engine, "color": self.color, "shift_gir": self.shift_gir}
+        new_car = {"name": self.name, "engine": self.engine, "color": self.color,
+                   "shift_gir": self.shift_gir}
         if len(data) > 0:
             new_car["id"] = data[-1]["id"] + 1
         else:
@@ -35,3 +51,32 @@ class Toyota:
         file = open(self.file, "w")
         data_in_json = json.dumps(data)
         file.write(data_in_json)
+
+
+    @classmethod
+    def change_color_car(cls):
+        Toyota.get_all_cars()
+        id_color = int(input("Enter ID car for change color: "))
+        new_color = input("Enter new color car: ")
+        data = cls.get_data()
+        counter = 0
+        for col in data:
+            counter = counter + 1
+            if id_color == counter:
+                col["color"] = new_color
+        file = open(cls.file, "w")
+        data_in_json = json.dumps(data)
+        file.write(data_in_json)
+
+    @classmethod
+    def get_car_engine(cls):
+        data = cls.get_data()
+        choice_engine = input("Select engine your prefer (diesel or petrol): ")
+        for col in data:
+            if col["engine"] == choice_engine:
+                print(col["id"], col["name"], col["engine"], col["color"], col["shift_gir"])
+        id_choice = int(input("Enter the id of the car you like: "))
+        for col in data:
+            if col["id"] == id_choice:
+                print("Your choice: ", col["id"], col["name"], col["engine"], col["color"], col["shift_gir"])
+
